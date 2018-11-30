2baaf9f344df132bac45eb20c90289c8ea1f6fde
Dave English <dave.english@digital.hmrc.gov.uk>
2018-11-30 15:01:48 +0000

    Add time workarounds try out

--- app/uk/gov/hmrc/gform/fileupload/Helper.scala
+++ app/uk/gov/hmrc/gform/fileupload/Helper.scala
@@ -57,7 +57,30 @@ class Helper(config: FUConfig, timeProvider: TimeProvider) {
     val timePlus: LocalDateTime = time.plusDays(expiryDays)
     val exp = timePlus.format(formatter)
     Logger.info(s"Envelope expiry current time ${time
-      .format(formatter)} expiry days $expiryDays and calculated expiry time ${exp.format(formatter)}")
+      .format(formatter)} expiry days $expiryDays and calculated expiry time ${timePlus.format(formatter)}")
+
+    // Time bug workaround tryouts
+
+    val timePlus2 = time.plusHours(expiryDays * 24)
+
+    Logger.info(s"time.plusHours(expiryDays * 24) ${timePlus2.format(formatter)}")
+
+    val timePlus3 = time.plusYears(1).minusDays(365 - expiryDays)  // This will yield one day more before and during a leap year
+
+    Logger.info(s"time.plusYears(1).minusDays(365 - expiryDays) ${timePlus3.format(formatter)}")
+
+    val timePlus4 = time.plusDays(expiryDays * 2).minusDays(expiryDays)  // this might go wrong at the start of November next year
+
+    Logger.info(s"time.plusDays(expiryDays * 2).minusDays(expiryDays) ${timePlus4.format(formatter)}")
+
+    val timePlus5 = time.minusYears(1).plusDays(expiryDays).plusYear(1) // this should work until 2020
+
+    Logger.info(s"time.minusYears(1).plusDays(expiryDays).plusYear(1) ${timePlus5.format(formatter)}")
+
+    val timePlus6 = time.plusHours(expiryDays * 24 - 1).plusHours(1)
+
+    Logger.info(s"time.plusHours(expiryDays * 24 - 1).plusHours(1) ${timePlus6.format(formatter)}")
+
     Logger.info("Setting fixed envelope expiry time as an emergency fix 2018-12-30T10:25:38Z")
     "2018-12-30T10:25:38Z" // TODO This is an emergency fix, only
   }
